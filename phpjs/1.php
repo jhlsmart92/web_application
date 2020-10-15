76e7551ec425f5013b71d05f581db7afee915e48
JEONGHAE LEE <71014763+jhlsmart92@users.noreply.github.com>
2020-10-15 19:36:15 +0900

    first commit

---
+++ phpjs/1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+<h1> php </h1>
+<body>
+    <?php 
+        echo 10+10;
+    ?>
+    <h1> javascript </h1>
+    <script>
+        document.write(10+10);
+    </script>
+</body>
+</html>
