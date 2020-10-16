90f8a52f6de283782b0a696e3454533b3b4452d4
JEONGHAE LEE <71014763+jhlsmart92@users.noreply.github.com>
2020-10-16 22:52:24 +0900

    Add files via upload

---
+++ phpjs/7.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php
+    $password = $_GET['password'];
+    if ($password == '1111'){
+    echo "hi";
+    } else {
+        echo "who are you";
+    }
+
+    ?>
+
+
+</body>
+</html>
